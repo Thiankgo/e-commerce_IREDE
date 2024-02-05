7fb25738bb97c87d22ef27eb94b14964b6d3079f
thiago.paiva <tdesousapaiva@gmail.com>
2024-02-05 11:15:14 -0300

    feat: pedidos button

--- src/pages/Pedidos/index.jsx
+++ src/pages/Pedidos/index.jsx
@@ -8,10 +8,11 @@ export default function Pedidos() {
         { key: 3, name: "Nique Air Surf", image: ProductImage, price: 24.99, category: "Tênis", status: "Finalizado" },
     ]
     return (
-        <main className="max-w-[1100px] flex flex-col justify-center m-auto p-6 gap-4">
-            <button className="relative bg-orange-500 font-[600] text-slate-100 rounded-lg w-[100%] h-[48px] max-w-[270px] mx-auto">Meus pedidos
+        <main className="max-w-[1100px] flex flex-col md:flex-row justify-center m-auto p-6 gap-4">
+            <button className="block md:hidden relative bg-orange-500 font-[600] text-slate-100 rounded-lg w-[100%] h-[48px] max-w-[270px] mx-auto">Meus pedidos
                 <IoMdArrowDropdown className="absolute w-8 h-8 right-[5%] top-[20%] m-auto"/>
             </button>
+            <div className="hidden md:block w-[250px] rounded bg-slate-100 text-orange-500"></div>
             <div className="bg-slate-100 p-3 flex flex-wrap gap-1 w-[100%] justify-items-start rounded-lg">
                 <h1 className="w-[100%] font-[600] text-[16px] text-black">Meus Pedidos</h1>
                 <div className="flex flex-wrap gap-1 lg:gap-8 justify-items-start">
