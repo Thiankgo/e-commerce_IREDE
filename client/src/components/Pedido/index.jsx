21552abeb6e7c53aa53a25e6f963945818c82e19
thiago.paiva <tdesousapaiva@gmail.com>
2024-03-04 09:53:15 -0300

    commit

--- client/src/components/Pedido/index.jsx
+++ client/src/components/Pedido/index.jsx
@@ -3,11 +3,11 @@ export default function Pedido({ pedido }) {
     return (
         <>
             <div className="flex flex-col md:flex-row w-full">
-                <div className="p-2 w-[210px] flex">
+                <div className="p-2 w-[230px] flex">
                     <img src={pedido?.image} alt={pedido?.name} className="w-[80px] h-[70px] rounded-lg object-cover" />
                     <div className="p-2 w-full">
-                        <h3 className="font-[600] text-[14px] black">{`${pedido?.name} Qnt: ${pedido?.quantity}`}</h3>
-                        <p className="text-stone-500 text-[10px] font-[600]">{pedido?.category}</p>
+                        <h3 className="font-[600] text-[14px] black">#{pedido?.id} {pedido?.name}</h3>
+                        <p className="text-stone-500 text-[10px] font-[600]">{pedido?.category} Qnt: {pedido?.quantity}</p>
                         <h4 className="font-[600] text-[12px] text-orange-500 mt-1">R$ {pedido?.price.toFixed(2).toLocaleString()}</h4>
                     </div>
                 </div>
